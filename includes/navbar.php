a0c0afae8c5e111e51f0dc3366b09aa8843a518c
Jake Taylor <jakeitaylor@hotmail.com>
2020-11-25 14:06:32 +0000

    Improved navigation bar icons

--- includes/navbar.php
+++ includes/navbar.php
@@ -13,141 +13,141 @@
                             <div class="sb-sidenav-menu-heading">Home</div>
                             <a class="nav-link" href="../../../index.php">
                                 <div class="sb-nav-link-icon">
-                                    <i class="fas fa-tachometer-alt"></i>
+                                    <i class="fa fa-home"></i>
                                 </div>
                                 Home
                             </a>
                             <a class="nav-link" href="../../../guild.php">
                                 <div class="sb-nav-link-icon">
-                                    <i class="fas fa-tachometer-alt"></i>
+                                    <i class="fa fa-users"></i>
                                 </div>
                                 Guild
                             </a>
                             <a class="nav-link" href="../../../event/leaderboard.php">
                                 <div class="sb-nav-link-icon">
-                                    <i class="fas fa-tachometer-alt"></i>
+                                    <i class="fa fa-star"></i>
                                 </div>
                                 Tournament
                             </a>
                             <a class="nav-link" href="../../../player.php">
                                 <div class="sb-nav-link-icon">
-                                    <i class="fas fa-tachometer-alt"></i>
+                                    <i class="fa fa-user"></i>
                                 </div>
                                 Player Statistics
                             </a>
                             <div class="sb-sidenav-menu-heading">Classic Leaderboards</div>
                             <a class="nav-link" href="../../../leaderboard/classic/guild/arena.php">
                                 <div class="sb-nav-link-icon">
-                                    <i class="fas fa-tachometer-alt"></i>
+                                    <i class="fa fa-th-list"></i>
                                 </div>
                                 Arena Brawl
                             </a>
                             <a class="nav-link" href="../../../leaderboard/classic/overall/paintball.php">
                                 <div class="sb-nav-link-icon">
-                                    <i class="fas fa-tachometer-alt"></i>
+                                    <i class="fa fa-th-list"></i>
                                 </div>
                                 Paintball
                             </a>
                             <a class="nav-link" href="../../../leaderboard/classic/overall/quakecraft.php">
                                 <div class="sb-nav-link-icon">
-                                    <i class="fas fa-tachometer-alt"></i>
+                                    <i class="fa fa-th-list"></i>
                                 </div>
                                 Quakecraft
                             </a>
                             <a class="nav-link" href="../../../leaderboard/classic/guild/tkr.php">
                                 <div class="sb-nav-link-icon">
-                                    <i class="fas fa-tachometer-alt"></i>
+                                    <i class="fa fa-th-list"></i>
                                 </div>
                                 Turbo Kart Racers
                             </a>
                             <a class="nav-link" href="../../../leaderboard/classic/guild/vampirez.php">
                                 <div class="sb-nav-link-icon">
-                                    <i class="fas fa-tachometer-alt"></i>
+                                    <i class="fa fa-th-list"></i>
                                 </div>
                                 VampireZ
                             </a>
                             <a class="nav-link" href="../../../leaderboard/classic/overall/walls.php">
                                 <div class="sb-nav-link-icon">
-                                    <i class="fas fa-tachometer-alt"></i>
+                                    <i class="fa fa-th-list"></i>
                                 </div>
                                 The Walls
                             </a>
                             <div class="sb-sidenav-menu-heading">Leaderboards</div>
                             <a class="nav-link" href="../error/404.php">
                                 <div class="sb-nav-link-icon">
-                                    <i class="fas fa-tachometer-alt"></i>
+                                    <i class="fa fa-th-list"></i>
                                 </div>
                                 Arcade
                             </a>
                             <a class="nav-link" href="../../../leaderboard/bedwars.php">
                                 <div class="sb-nav-link-icon">
-                                    <i class="fas fa-tachometer-alt"></i>
+                                    <i class="fa fa-th-list"></i>
                                 </div>
                                 BedWars
                             </a>
                             <a class="nav-link" href="../error/404.php">
                                 <div class="sb-nav-link-icon">
-                                    <i class="fas fa-tachometer-alt"></i>
+                                    <i class="fa fa-th-list"></i>
                                 </div>
                                 Blitz Survival Games
                             </a>
                             <a class="nav-link" href="../error/404.php">
                                 <div class="sb-nav-link-icon">
-                                    <i class="fas fa-tachometer-alt"></i>
+                                    <i class="fa fa-th-list"></i>
                                 </div>
                                 Build Battle
                             </a>
                             <a class="nav-link" href="../error/404.php">
                                 <div class="sb-nav-link-icon">
-                                    <i class="fas fa-tachometer-alt"></i>
+                                    <i class="fa fa-th-list"></i>
                                 </div>
                                 Cops and Crims
                             </a>
                             <a class="nav-link" href="../error/404.php">
                                 <div class="sb-nav-link-icon">
-                                    <i class="fas fa-tachometer-alt"></i>
+                                    <i class="fa fa-th-list"></i>
                                 </div>
                                 Duels
                             </a>
                             <a class="nav-link" href="../error/404.php">
                                 <div class="sb-nav-link-icon">
-                                    <i class="fas fa-tachometer-alt"></i>
+                                    <i class="fa fa-th-list"></i>
                                 </div>
                                 Mega Walls
                             </a>
                             <a class="nav-link" href="../error/404.php">
                                 <div class="sb-nav-link-icon">
-                                    <i class="fas fa-tachometer-alt"></i>
+                                    <i class="fa fa-th-list"></i>
                                 </div>
                                 Murder Mystery
                             </a>
                             <a class="nav-link" href="../../../leaderboard/skywars.php">
                                 <div class="sb-nav-link-icon">
-                                    <i class="fas fa-tachometer-alt"></i>
+                                    <i class="fa fa-th-list"></i>
                                 </div>
                                 SkyWars
                             </a>
                             <a class="nav-link" href="../error/404.php">
                                 <div class="sb-nav-link-icon">
-                                    <i class="fas fa-tachometer-alt"></i>
+                                    <i class="fa fa-th-list"></i>
                                 </div>
                                 Smash Heroes
                             </a>                            
                             <a class="nav-link" href="../../../leaderboard/tntgames.php">
                                 <div class="sb-nav-link-icon">
-                                    <i class="fas fa-tachometer-alt"></i>
+                                    <i class="fa fa-th-list"></i>
                                 </div>
                                 TNT Games
                             </a>
                             <a class="nav-link" href="../error/404.php">
                                 <div class="sb-nav-link-icon">
-                                    <i class="fas fa-tachometer-alt"></i>
+                                    <i class="fa fa-th-list"></i>
                                 </div>
                                 UHC
                             </a>
                             <a class="nav-link" href="../../../leaderboard/warlords.php">
                                 <div class="sb-nav-link-icon">
-                                    <i class="fas fa-tachometer-alt"></i>
+                                    <i class="fa fa-th-list"></i>
                                 </div>
                                 Warlords
                             </a>
